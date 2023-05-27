0fb9d05bc5daadcdab99405ea8ac2f9ea0e93857
6040H0K <ddogonov3@gmail.com>
2023-05-27 13:28:42 +0300

    Add controls for os

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from recognize.rec import *
 from response.init import *
 
+#Good dumplings are very very tasty
 
 while True:
     # старт записи речи с последующим выводом распознанной речи 
     voice_input = record_and_recognize_audio()
     print(voice_input)
-    for tolk in list_tolking:
-        for req in tolk.request:
-            if req in voice_input:
-                tolk.response(voice_input)
-                break
+    if voice_input == 'стоп':
+        break
+    else:
+        for tolk in list_tolking:
+            for req in tolk.request:
+                if req in voice_input:
+                    tolk.response(voice_input)
+                    break
 
     tts_engine.runAndWait()
--- response/init.py
+++ response/init.py
@@ -4,3 +4,4 @@ from response.currency.currency import *
 from response.weather_and_news.weather import *
 from response.weather_and_news.news import *
 from response.browser.googleSearch import *
+from response.controlos.controlos import *
